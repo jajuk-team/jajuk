f7fc1a36892527aa5a05630559e96818e8f07f69
Bertrand Florat <bertrand@florat.net>
2004-03-30 19:25:11 +0000

    30/03/2004

--- jajuk/scripts/jajuk.sh
+++ jajuk/scripts/jajuk.sh
@@ -2,5 +2,5 @@
 #Jajuk launching script for unix, assumes javaw program is in the PATH env. variable
 #Go to installation directory we get with own shell path
 cd `dirname $0`/bin
-#Lauch jajuk
+#Launch jajuk
 java -client -Xms25M -jar jajuk.jar 
