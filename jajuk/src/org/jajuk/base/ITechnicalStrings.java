a927c0f122ea757e242650ebb949adb8941dc834
Bertrand Florat <bertrand@florat.net>
2004-06-12 10:28:40 +0000

    12/06/2004

--- jajuk/src/org/jajuk/base/ITechnicalStrings.java
+++ jajuk/src/org/jajuk/base/ITechnicalStrings.java
@@ -31,7 +31,7 @@ import org.jajuk.util.Util;
 public interface ITechnicalStrings {
 	
 	// Jjauk version
-	public static final String JAJUK_VERSION = "0.2";
+	public static final String JAJUK_VERSION = "0.2.1";
 	
 	//About 
 	public static final String ABOUT = "<html>Jajuk version "+JAJUK_VERSION+"</html>";
