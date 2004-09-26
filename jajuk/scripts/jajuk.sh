19fef39ba40544eca75e7bfd2b32d7c5106215d3
Bertrand Florat <bertrand@florat.net>
2004-09-26 20:48:07 +0000

    Refactoring

--- jajuk/scripts/jajuk.sh
+++ jajuk/scripts/jajuk.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 #Jajuk launching script for unix, assumes javaw program is in the PATH env. variable
+
 #Go to installation directory we get with own shell path
 cd `dirname $0`/bin
+
+#Set right JAVA_HOME variable according to JRE runtime
+export JAVA_HOME=`java -cp jajuk.jar org.jajuk.SystemAnalyser`
+
 #Launch jajuk
-java -client -Xms25M -jar jajuk.jar 
+java -client -Xms25M -Djava.library.path=../native -jar jajuk.jar -notaskbar
