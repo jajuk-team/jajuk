eb1b4f38cf354bd4e9c50031893fbb90895272b1
Bertrand Florat <bertrand@florat.net>
2004-12-10 13:50:51 +0000

    Initial message fix : no technical string

--- jajuk/src/org/jajuk/ui/views/AnimationView.java
+++ jajuk/src/org/jajuk/ui/views/AnimationView.java
@@ -31,8 +31,9 @@ import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 
 import org.jajuk.Main;
+import org.jajuk.base.FIFO;
+import org.jajuk.base.File;
 import org.jajuk.i18n.Messages;
-import org.jajuk.ui.InformationJPanel;
 import org.jajuk.ui.ObservationManager;
 import org.jajuk.ui.Observer;
 import org.jajuk.util.ITechnicalStrings;
@@ -90,10 +91,10 @@ public class AnimationView extends ViewAdapter implements ITechnicalStrings,Obse
 		setForeground(Color.BLACK);
 		add(btl1);
 		
-		ObservationManager.register(EVENT_INFORMATION_DISPLAY,this);
+		ObservationManager.register(EVENT_FILE_LAUNCHED,this);
 		ObservationManager.register(EVENT_ZERO,this);
 		//check if a track has already been lauched
-		update(EVENT_INFORMATION_DISPLAY);
+		update(EVENT_FILE_LAUNCHED);
 	}
 	
 	/**Set the text to be displayed**/
@@ -146,11 +147,16 @@ public class AnimationView extends ViewAdapter implements ITechnicalStrings,Obse
 	 * @see org.jajuk.ui.Observer#update(java.lang.String)
 	 */
 	public void update(String subject) {
-		if (subject.equals(EVENT_INFORMATION_DISPLAY)){
-			String s = InformationJPanel.getInstance().getMessage();
-			if (s != null  && !s.trim().equals("")){ //$NON-NLS-1$
-				setText(s);   
-			}
+		if (subject.equals(EVENT_FILE_LAUNCHED)){
+		    File file = FIFO.getInstance().getCurrentFile();
+		    if (file != null){
+		        String s = Messages.getString("FIFO.10")+file.getTrack().getAuthor().getName2() //$NON-NLS-1$
+		        +" / "+file.getTrack().getAlbum().getName2()+" / " //$NON-NLS-1$ //$NON-NLS-2$
+		        +file.getTrack().getName();//$NON-NLS-1$ //$N
+		        if (s != null  && !s.trim().equals("")){ //$NON-NLS-1$
+		            setText(s);   
+		        }
+		    }
 		}
 		else if (subject.equals(EVENT_ZERO)){
 		    setText(Messages.getString("JajukWindow.18")); //$NON-NLS-1$
@@ -165,7 +171,7 @@ public class AnimationView extends ViewAdapter implements ITechnicalStrings,Obse
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				iSize = SwingUtilities.getRootPane(AnimationView.this).getWidth(); //current  width
-				update(EVENT_INFORMATION_DISPLAY);//force redisplay
+				update(EVENT_FILE_LAUNCHED);//force redisplay
 			}
 		});
 		Log.debug("View resized, new width="+iSize); //$NON-NLS-1$
