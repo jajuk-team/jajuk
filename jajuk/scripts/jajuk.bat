b075fb199dcee99dd4915d259d69d29c47142ebb
Bertrand Florat <bertrand@florat.net>
2005-02-18 22:49:04 +0000

    Removed Xss JVM setting

--- jajuk/scripts/jajuk.bat
+++ jajuk/scripts/jajuk.bat
@@ -1,3 +1,3 @@
 REM Jajuk launching script for Windows, assumes javaw program is in the PATH env. variable
 cd bin
-java -client -Xms25M -Xss5M -Djava.library.path=..\native -jar jajuk.jar  -notaskbar
+java -client -Xms25M  -Djava.library.path=..\native -jar jajuk.jar  -notaskbar
