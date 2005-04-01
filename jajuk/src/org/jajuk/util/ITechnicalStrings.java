ff0a3501192dc24f571c1dfecf69c226f776a821
Bertrand Florat <bertrand@florat.net>
2005-04-01 19:42:02 +0000

    *** empty log message ***

--- jajuk/src/org/jajuk/util/ITechnicalStrings.java
+++ jajuk/src/org/jajuk/util/ITechnicalStrings.java
@@ -48,8 +48,8 @@ public interface ITechnicalStrings {
     
     		
 	// Jajuk version
-	public static final String JAJUK_VERSION = "0.3.3 rc14";
-	public static final String JAJUK_VERSION_DATE = "Build 2005-03-";
+	public static final String JAJUK_VERSION = "0.3.3";
+	public static final String JAJUK_VERSION_DATE = "Build 2005-04-02";
 	
 	//About 
 	public static final String ABOUT = "<html>Jajuk version "+JAJUK_VERSION+"</html>";
