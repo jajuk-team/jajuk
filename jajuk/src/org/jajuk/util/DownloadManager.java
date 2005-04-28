320ca5e6001ef6b7e4444cfc19f29cead0099337
Bertrand Florat <bertrand@florat.net>
2005-04-28 17:46:22 +0000

    More cover fixes

--- jajuk/src/org/jajuk/util/DownloadManager.java
+++ jajuk/src/org/jajuk/util/DownloadManager.java
@@ -33,7 +33,6 @@ import org.apache.commons.httpclient.UsernamePasswordCredentials;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.jajuk.Main;
 import org.jajuk.i18n.Messages;
-import org.jajuk.util.error.JajukException;
 import org.jajuk.util.log.Log;
 
 /**
@@ -98,51 +97,43 @@ public class DownloadManager implements ITechnicalStrings {
 	 * @param search
 	 * @return a list of urls
 	 */
-	public static ArrayList getRemoteCoversList(String search) throws JajukException{
-		ArrayList alOut = new ArrayList(20); //URL list   
-	    try{
-	        String sSearchUrl = "http://images.google.com/images?q="+URLEncoder.encode(search, "ISO-8859-1")+"&ie=ISO-8859-1&hl=en&btnG=Google+Search"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-	        Log.debug("Search URL: "+sSearchUrl); //$NON-NLS-1$
-	        byte[] bRes = download(new URL(sSearchUrl));
-	        if (bRes == null || bRes.length==0){
-	            return alOut;
-	        }
-	        String sRes = new String(bRes);
-	        //get urls
-	        String[] strings = sRes.split("imgurl="); //$NON-NLS-1$
-	        for (int i=1;i<strings.length;i++){
-	            String s = strings[i].substring(0,strings[i].indexOf('&')); 
-	            s = s.replaceAll("%2520","%20"); //$NON-NLS-1$ //$NON-NLS-2$
-	            alOut.add(new URL(s));
-	        }
-	        //get sizes
-	        strings = sRes.split("pixels - "); //$NON-NLS-1$
-	        int iNbRemove = 0; //removes number used to compute new index
-	        for (int i=1;i<strings.length;i++){
-	            int iKPos = strings[i].indexOf('k');
-	            int iSize = Integer.parseInt(strings[i].substring(0,iKPos));
-	            if ( iSize > ConfigurationManager.getInt(CONF_COVERS_MAX_SIZE) ||  iSize > MAX_COVER_SIZE || iSize < ConfigurationManager.getInt(CONF_COVERS_MIN_SIZE) ){
-	                alOut.remove(i-(1+iNbRemove));
-	                iNbRemove++;
-	            }
-	        }
-	    }
-	    catch(JajukException je){  //concurrent exception 
-	        throw je;
-	    }
-	    catch(Exception e){
-	        Log.debug(e.toString());
-	    }
-	    return alOut;
-	}
-	
+    public static ArrayList getRemoteCoversList(String search) throws Exception{
+        ArrayList alOut = new ArrayList(20); //URL list   
+        String sSearchUrl = "http://images.google.com/images?q="+URLEncoder.encode(search, "ISO-8859-1")+"&ie=ISO-8859-1&hl=en&btnG=Google+Search"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        Log.debug("Search URL: "+sSearchUrl); //$NON-NLS-1$
+        byte[] bRes = download(new URL(sSearchUrl));
+        if (bRes == null || bRes.length==0){
+            return alOut;
+        }
+        String sRes = new String(bRes);
+        //get urls
+        String[] strings = sRes.split("imgurl="); //$NON-NLS-1$
+        for (int i=1;i<strings.length;i++){
+            String s = strings[i].substring(0,strings[i].indexOf('&')); 
+            s = s.replaceAll("%2520","%20"); //$NON-NLS-1$ //$NON-NLS-2$
+            alOut.add(new URL(s));
+        }
+        //get sizes
+        strings = sRes.split("pixels - "); //$NON-NLS-1$
+        int iNbRemove = 0; //removes number used to compute new index
+        for (int i=1;i<strings.length;i++){
+            int iKPos = strings[i].indexOf('k');
+            int iSize = Integer.parseInt(strings[i].substring(0,iKPos));
+            if ( iSize > ConfigurationManager.getInt(CONF_COVERS_MAX_SIZE) ||  iSize > MAX_COVER_SIZE || iSize < ConfigurationManager.getInt(CONF_COVERS_MIN_SIZE) ){
+                alOut.remove(i-(1+iNbRemove));
+                iNbRemove++;
+            }
+        }
+        return alOut;
+    }
+    
 	/**
 	 * Download the resource at the given url
 	 * @param url to download
-	 * @throws JajukException if a concurrent connection is alive
+	 * @throws Exception
 	 * @return result as an array of bytes, null if a problem occured
 	 */
-	public static byte[] download(URL url) throws JajukException{
+	public static byte[] download(URL url) throws Exception{
 	    byte[] bOut = null;
 	    GetMethod get = null;
 	    try{
@@ -162,9 +153,8 @@ public class DownloadManager implements ITechnicalStrings {
 	        bOut = get.getResponseBody();
 	     }
 	    catch(Exception e){
-	        e.printStackTrace();
 	        Log.debug("Time out during cover lookup"); //$NON-NLS-1$
-	    	throw new JajukException("129"); //mainly time outs //$NON-NLS-1$
+	    	throw e;
 	    }
 	    finally{
 	        get.releaseConnection();
