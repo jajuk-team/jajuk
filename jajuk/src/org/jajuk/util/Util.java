4ea261c754d4d693e9fa00fa7be760d14987941a
Bertrand Florat <bertrand@florat.net>
2005-06-03 18:24:53 +0000

    Code for LAF upload

--- jajuk/src/org/jajuk/util/Util.java
+++ jajuk/src/org/jajuk/util/Util.java
@@ -1,6 +1,9 @@
 /*
  *  Jajuk
  *  Copyright (C) 2003 Bertrand Florat
+ *  
+ *  Methods updateAllUIs(), updateComponentTreeUI() and updateComponentTreeUIO() thanks BigLee in this forum:
+ *  http://forum.java.sun.com/thread.jspa?threadID=596251&messageID=3160062
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
@@ -23,7 +26,9 @@ import java.awt.Component;
 import java.awt.Container;
 import java.awt.Cursor;
 import java.awt.Dimension;
+import java.awt.Frame;
 import java.awt.Image;
+import java.awt.Window;
 import java.awt.image.AreaAveragingScaleFilter;
 import java.awt.image.FilteredImageSource;
 import java.awt.image.ImageFilter;
@@ -53,7 +58,9 @@ import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JComponent;
+import javax.swing.JMenu;
 import javax.swing.JPanel;
+import javax.swing.JToolBar;
 import javax.swing.SwingUtilities;
 
 import org.jajuk.Main;
@@ -66,6 +73,7 @@ import org.jajuk.ui.JajukContainer;
 import org.jajuk.ui.PerspectiveBarJPanel;
 import org.jajuk.ui.perspectives.IPerspective;
 import org.jajuk.ui.perspectives.PerspectiveManager;
+import org.jajuk.ui.tray.JajukSystray;
 import org.jajuk.util.error.JajukException;
 import org.jajuk.util.log.Log;
 
@@ -828,4 +836,99 @@ public class Util implements ITechnicalStrings {
         return img;
     }
 	
+    
+    /**
+     * Method to attempt a dynamic update for any GUI accessible by this JVM. It will
+     * filter through all frames and sub-components of the frames.
+     */
+    public static void updateAllUIs()
+    {
+        Frame frames[];
+        int i1;
+        frames = Frame.getFrames();
+        i1 = 0;
+ 
+        for (int i = 0; i < frames.length; i++)
+        {
+            updateWindowUI(frames[i]);
+        }
+        //update tray
+        updateComponentTreeUI(JajukSystray.getInstance().getPopup());
+    }
+ 
+    /**
+     * Method to attempt a dynamic update for all components of the given <code>Window</code>.
+     * @param window The <code>Window</code> for which the look and feel update has to be performed against.
+     */
+    public static void updateWindowUI(Window window)
+    {
+        try
+        {
+            updateComponentTreeUI(window);
+        }
+        catch(Exception exception) { }
+ 
+        Window windows[] = window.getOwnedWindows();
+ 
+        for (int i = 0; i < windows.length; i++)
+            updateWindowUI(windows[i]);
+    }
+ 
+    /**
+     * A simple minded look and feel change: ask each node in the tree
+     * to <code>updateUI()</code> -- that is, to initialize its UI property
+     * with the current look and feel.
+     *
+     * Based on the Sun SwingUtilities.updateComponentTreeUI, but ensures that
+     * the update happens on the components of a JToolbar before the JToolbar
+     * itself.
+     */
+    public static void updateComponentTreeUI(Component c) {
+        updateComponentTreeUI0(c);
+        c.invalidate();
+        c.validate();
+        c.repaint();
+    }
+ 
+    private static void updateComponentTreeUI0(Component c) {
+ 
+        Component[] children = null;
+ 
+        if (c instanceof JToolBar)
+        {
+            children = ((JToolBar)c).getComponents();
+ 
+            if (children != null)
+            {
+                for(int i = 0; i < children.length; i++)
+                {
+                    updateComponentTreeUI0(children[i]);
+                }
+            }
+ 
+            ((JComponent) c).updateUI();
+        }
+        else
+        {
+            if (c instanceof JComponent) {
+                ((JComponent) c).updateUI();
+            }
+ 
+            if (c instanceof JMenu) {
+                children = ((JMenu)c).getMenuComponents();
+            }
+            else if (c instanceof Container) {
+                children = ((Container)c).getComponents();
+            }
+ 
+            if (children != null)
+            {
+                for(int i = 0; i < children.length; i++)
+                {
+                    updateComponentTreeUI0(children[i]);
+                }
+            }
+        }
+    }
+ 
 }
