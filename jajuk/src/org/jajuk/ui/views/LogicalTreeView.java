981ee6e6f758895782a48e12855a199caa4c4677
Bertrand Florat <bertrand@florat.net>
2006-01-26 23:59:45 +0000

    Sort even styles

--- jajuk/src/org/jajuk/ui/views/LogicalTreeView.java
+++ jajuk/src/org/jajuk/ui/views/LogicalTreeView.java
@@ -601,7 +601,7 @@ ActionListener, Observer {
         synchronized (TrackManager.getInstance().getLock()) {
             tracks = new ArrayList(TrackManager.getInstance().getItems());
         }
-        //no need for sorting as we use natural ordering of tracks
+        Collections.sort(tracks,TrackManager.getInstance().getComparator());
         for (Track track : tracks) {
             if (!track.shouldBeHidden()) {
                 StyleNode styleNode = null;
