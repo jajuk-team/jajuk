acc7a39d26a836174603a2822b7d1c5b996ef7d9
Bertrand Florat <bertrand@florat.net>
2006-05-25 21:39:50 +0000

    Complete refactoring

--- jajuk/src/org/jajuk/util/JajukFileFilter.java
+++ jajuk/src/org/jajuk/util/JajukFileFilter.java
@@ -35,81 +35,289 @@ import org.jajuk.base.TypeManager;
  * @created    22 oct. 2003
  */
 
-public class JajukFileFilter extends FileFilter implements java.io.FileFilter{
+public class JajukFileFilter extends FileFilter implements java.io.FileFilter,ITechnicalStrings{
 	/**Display directories flag**/
 	private boolean bDirectories = true;
 	/**Display files flag**/
 	private boolean bFiles = true;
-	/**Display audio files**/
-	private boolean bAudio = true;
 	/**Accepted types**/
-	private ArrayList alTypes;
+	private ArrayList<Type> alTypes;
+    /**Filters*/
+    private java.io.FileFilter[] filters; 
+    /**And or OR applied to multi filters ?*/
+    private boolean bAND = true;
+	
+    
+    /**
+     * 
+     *  Directory filter
+     *  <p>Singleton</p>
+     *
+     * @author     Bertrand Florat
+     * @created    25 may 2006
+     */
+    public static class DirectoryFilter implements java.io.FileFilter{
+        
+        /**Self instance*/
+        private static DirectoryFilter self = null;
+        
+        /* (non-Javadoc)
+         * @see java.io.FileFilter#accept(java.io.File)
+         */
+        public boolean accept(File f) {
+            return f.isDirectory();
+        }
+        
+        /**No instanciation*/
+        private DirectoryFilter(){};
+        
+        /**
+         * 
+         * @return singleton
+         */
+        public static DirectoryFilter getInstance(){
+            if (self == null){
+                self = new DirectoryFilter();
+            }
+            return self;
+        }
+    }
+    
+    /**
+     * 
+     *  Any File filter
+     *  <p>Singleton</p>
+     * @author     Bertrand Florat
+     * @created    25 may 2006
+     */
+    public static class AnyFileFilter implements java.io.FileFilter{
+        /**Self instance*/
+        private static AnyFileFilter self = null;
+        
+        /* (non-Javadoc)
+         * @see java.io.FileFilter#accept(java.io.File)
+         */
+        public boolean accept(File f) {
+            return !f.isDirectory();
+        }
+        
+        /**No instanciation*/
+        private AnyFileFilter(){};
+        
+        /**
+         * 
+         * @return singleton
+         */
+        public static AnyFileFilter getInstance(){
+            if (self == null){
+                self = new AnyFileFilter();
+            }
+            return self;
+        }
+    }
+
+    /**
+     * 
+     *  Known type filter
+     *
+     * @author     Bertrand Florat
+     * @created    25 may 2006
+     */
+    public static class KnownTypeFilter implements java.io.FileFilter{
+        /**Self instance*/
+        private static KnownTypeFilter self = null;
+        
+        /* (non-Javadoc)
+         * @see java.io.FileFilter#accept(java.io.File)
+         */
+        public boolean accept(File f) {
+            if (f.isDirectory()){
+                return false;
+            }
+            return TypeManager.getInstance().isExtensionSupported(Util.getExtension(f));
+        }
+        
+        /**No instanciation*/
+        private KnownTypeFilter(){};
+        
+        /**
+         * 
+         * @return singleton
+         */
+        public static KnownTypeFilter getInstance(){
+            if (self == null){
+                self = new KnownTypeFilter();
+            }
+            return self;
+        }
+    }
+    
+    /**
+     * 
+     *  Audio filter
+     *
+     * @author     Bertrand Florat
+     * @created    25 may 2006
+     */
+    public static class AudioFilter implements java.io.FileFilter{
+        /**Self instance*/
+        private static AudioFilter self = null;
+        
+        /* (non-Javadoc)
+         * @see java.io.FileFilter#accept(java.io.File)
+         */
+        public boolean accept(File f) {
+            if (f.isDirectory()){
+                return false;
+            }
+            //check extension is known
+            if (TypeManager.getInstance().isExtensionSupported(Util.getExtension(f))){
+                //check it is an audio file
+                return (Boolean)TypeManager.getInstance().getTypeByExtension(Util.getExtension(f)).getValue(XML_TYPE_IS_MUSIC);
+            }
+            return false;
+        }
+        
+        /**No instanciation*/
+        private AudioFilter(){};
+        
+        /**
+         * 
+         * @return singleton
+         */
+        public static AudioFilter getInstance(){
+            if (self == null){
+                self = new AudioFilter();
+            }
+            return self;
+        }
+    }
+    
+    /**
+     * 
+     *  Not Audio file filter (must be a file)
+     *
+     * @author     Bertrand Florat
+     * @created    25 may 2006
+     */
+    public static class NotAudioFilter implements java.io.FileFilter{
+        /**Self instance*/
+        private static NotAudioFilter self = null;
+        
+        /* (non-Javadoc)
+         * @see java.io.FileFilter#accept(java.io.File)
+         */
+        public boolean accept(File f) {
+            if (f.isDirectory()){
+                return false;
+            }
+            //check extension is known
+            if (TypeManager.getInstance().isExtensionSupported(Util.getExtension(f))){
+                //check it is an audio file
+                return !(Boolean)TypeManager.getInstance().getTypeByExtension(Util.getExtension(f)).getValue(XML_TYPE_IS_MUSIC);
+            }
+            //unknown type : not an audio file
+            return true;
+        }
+        
+        /**No instanciation*/
+        private NotAudioFilter(){};
+        
+        /**
+         * 
+         * @return singleton
+         */
+        public static NotAudioFilter getInstance(){
+            if (self == null){
+                self = new NotAudioFilter();
+            }
+            return self;
+        }
+    }
+    
+    /**
+     * 
+     *  Playlist filter
+     *
+     * @author     Bertrand Florat
+     * @created    25 may 2006
+     */
+    public static class PlaylistFilter implements java.io.FileFilter{
+        /**Self instance*/
+        private static PlaylistFilter self = null;
+        
+        /* (non-Javadoc)
+         * @see java.io.FileFilter#accept(java.io.File)
+         */
+        public boolean accept(File f) {
+            if (f.isDirectory()){
+                return false;
+            }
+            //check extension is known
+            if (TypeManager.getInstance().isExtensionSupported(Util.getExtension(f))){
+                //check it is a playlist
+                Type playlist = TypeManager.getInstance().getTypeByExtension(EXT_PLAYLIST);
+                return (Boolean)TypeManager.getInstance().getTypeByExtension(Util.getExtension(f)).equals(playlist);
+            }
+            return false;
+        }
+        
+        /**No instanciation*/
+        private PlaylistFilter(){};
+        
+        /**
+         * 
+         * @return singleton
+         */
+        public static PlaylistFilter getInstance(){
+            if (self == null){
+                self = new PlaylistFilter();
+            }
+            return self;
+        }
+    }
+
+
+    
+    /**
+     * Filter constructor
+     * @param filters undefined list of jajuk filter to be applied (logical AND applied between filters)
+     * <p>Example: only audio files new JajukFilter(JajukFileFilter.AudioFilter.getInstance());
+     * </p>
+     */
+    public JajukFileFilter(java.io.FileFilter... filters ){
+        this(true,filters);
+    }
+    
+    /**
+     * Filter constructor
+     * @param filters undefined list of jajuk filter to be applied (logical AND applied between filters)
+     * @param bAND: should be applied an AND or an OR between filters ?
+     * <p>Example: audio files or directories: new JajukFilter(false,JajukFileFilter.DirectoryFilter.getInstance(),JajukFileFilter.AudioFilter.getInstance());
+     * </p>
+     */
+    public JajukFileFilter(boolean bAND,java.io.FileFilter... filters ){
+        this.bAND = bAND;
+        this.filters = filters;
+    }
 	
 	/**
-	 * Constructor
-	 * @param bDirectories can we show directories
-	 * @param bFiles can we show files
-	 * @param types which type do we show
-	 */	
-	public JajukFileFilter(boolean bDirectories,ArrayList alTypes){
-		this.bDirectories = bDirectories; 
-		this.alTypes = alTypes;
-	}
-
-	/**
-	 * Constructor, no type specified
-	 * @param bDirectories
-	 * @param bFiles
+     * Return file test. Apply all filters with a logical AND
+     * @param f file to test 
 	 */
-	public  JajukFileFilter(boolean bDirectories,boolean bFiles){
-		this.bDirectories = bDirectories;
-		this.bFiles = bFiles;
-	}
-
-	/**
-	 * Default Constructor, true for files, true for directories
-	 *
-	 */
-	public JajukFileFilter(){
-		this(true,true);
-	}
-
-	/**
-	 * Constructor
-	 * @param bDirectories can we show directories
-	 * @param bFiles can we show files
-	 * @param bAudio can we show audio files
-	 */
-	public JajukFileFilter(boolean bAudio){
-		this.bDirectories = false;
-		this.bFiles = true;
-		this.bAudio = bAudio;
-		
-	}
-	
-	/**Tells if a file is selected or not**/
-	public boolean accept(File f) {
-		//directories case
-		if ( bDirectories && f.isDirectory()) {				
-			return true;
-		}		
-		//file cases
-		if ( alTypes != null){ //one or more types is specified
-			Iterator it = alTypes.iterator();
-			while (it.hasNext()){
-				Type type = (Type)it.next();
-				if ( Util.getExtension(f).equals(type.getExtension())){
-					return true;
-				}
-			}
-		} else{
-			if (!bAudio && TypeManager.getInstance().isExtensionSupported(Util.getExtension(f))){
-				return false;
-			} else if (bFiles){
-				return true;
-			}
+    public boolean accept(File f) {
+		boolean bResu = true;
+		if (bAND){
+		    for (int i=0;i<filters.length;i++){
+		        bResu = bResu & filters[i].accept(f);
+		    }
 		}
-		return false;
+		else{ //OR applied
+            for (int i=0;i<filters.length;i++){
+                bResu = bResu | filters[i].accept(f);
+            }
+        }
+	    return bResu;
 	}
 	
 	
