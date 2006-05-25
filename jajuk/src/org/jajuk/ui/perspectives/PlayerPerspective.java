a6d2175e36a8a1dac7b17044193ef907915bcc24
Bertrand Florat <bertrand@florat.net>
2006-05-25 21:42:43 +0000

    New playlist editor view

--- jajuk/src/org/jajuk/ui/perspectives/PlayerPerspective.java
+++ jajuk/src/org/jajuk/ui/perspectives/PlayerPerspective.java
@@ -25,10 +25,13 @@ import java.io.IOException;
 import net.infonode.docking.SplitWindow;
 import net.infonode.docking.util.ViewMap;
 
+import org.jajuk.base.PlaylistFile;
 import org.jajuk.i18n.Messages;
+import org.jajuk.ui.PlaylistFileItem;
 import org.jajuk.ui.views.AnimationView;
 import org.jajuk.ui.views.CoverView;
 import org.jajuk.ui.views.IView;
+import org.jajuk.ui.views.LogicalPlaylistEditorView;
 
 /**
  *  Player perspective, contains view usefull to be displayed duriong playing
@@ -57,11 +60,17 @@ public class PlayerPerspective extends PerspectiveAdapter {
 	    net.infonode.docking.View dockingCoverView2 = addView(view);
 		viewMap.addView(4,dockingCoverView2);
 		
-		SplitWindow vertCoversSplit = new SplitWindow(true,0.5f,dockingCoverView1,dockingCoverView2);
-        SplitWindow horMainSplit = new SplitWindow(false,0.2f,dockingAnimationView,vertCoversSplit);
+		LogicalPlaylistEditorView editor = new LogicalPlaylistEditorView();
+		PlaylistFileItem plfiQueue = new PlaylistFileItem(PlaylistFileItem.PLAYLIST_TYPE_QUEUE,ICON_PLAYLIST_QUEUE,new PlaylistFile(PlaylistFileItem.PLAYLIST_TYPE_QUEUE,"1",null,null),Messages.getString("PhysicalPlaylistRepositoryView.9")); //$NON-NLS-1$ //$NON-NLS-2$
+        editor.setCurrentPlaylistFileItem(plfiQueue);   //set playlist to queue 
+        net.infonode.docking.View dockingEditor = addView(editor);
+        viewMap.addView(5,dockingEditor);
+        
+        SplitWindow vertCoversSplit = new SplitWindow(true,0.5f,dockingCoverView1,dockingCoverView2);
+        SplitWindow vertSplit1 = new SplitWindow(true,0.25f,dockingEditor,vertCoversSplit);
+        SplitWindow horMainSplit = new SplitWindow(false,0.2f,dockingAnimationView,vertSplit1);
 		
         setRootWindow(viewMap,horMainSplit);
-        
         
 	}
 	
