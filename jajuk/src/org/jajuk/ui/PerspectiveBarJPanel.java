763fb75269d328330660f3605f586c61330888e8
Bertrand Florat <bertrand@florat.net>
2006-10-15 21:10:41 +0000

    New perspective bar

--- jajuk/src/org/jajuk/ui/PerspectiveBarJPanel.java
+++ jajuk/src/org/jajuk/ui/PerspectiveBarJPanel.java
@@ -19,23 +19,21 @@
  */
 package org.jajuk.ui;
 
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
-import javax.swing.JToolBar;
 
 import org.jajuk.i18n.Messages;
 import org.jajuk.ui.perspectives.IPerspective;
 import org.jajuk.ui.perspectives.PerspectiveManager;
 import org.jajuk.util.ITechnicalStrings;
 import org.jajuk.util.Util;
+
+import com.l2fprod.common.swing.JButtonBar;
 
 /**
  * Menu bar used to choose the current perspective.
@@ -47,7 +45,7 @@ public class PerspectiveBarJPanel extends JPanel implements ITechnicalStrings{
     private static final long serialVersionUID = 1L;
 
     /** Perspectives tool bar**/
-    private JToolBar jtbPerspective = null;
+    private JButtonBar jtbPerspective = null;
 
 	/**Self instance*/
 	static private PerspectiveBarJPanel pb = null;
@@ -55,7 +53,7 @@ public class PerspectiveBarJPanel extends JPanel implements ITechnicalStrings{
     /**
      * Perspective button
      */
-    private ArrayList alButtons = new ArrayList(10);
+    private ArrayList<JButton> alButtons = new ArrayList<JButton>(10);
 
  
 	
@@ -84,22 +82,15 @@ public class PerspectiveBarJPanel extends JPanel implements ITechnicalStrings{
 	 *
 	 */
 	public void update(){
-		// set default layout and size
-		setLayout(new BoxLayout(this,BoxLayout.Y_AXIS)); //we use a BoxLayout and not a FlowLayout to allow resizing
-		setBorder(BorderFactory.createEmptyBorder(4,4,4,4));
 		// Perspectives tool bar
-		jtbPerspective = new JToolBar();
-		jtbPerspective.setFloatable(false);
-		jtbPerspective.addSeparator();
-		jtbPerspective.setOrientation(JToolBar.VERTICAL);
-	
+		jtbPerspective = new JButtonBar(JButtonBar.VERTICAL);
 		Iterator it = PerspectiveManager.getPerspectives().iterator();
+        int index = 0;
 		while ( it.hasNext()){
 			final IPerspective perspective = (IPerspective)it.next();
 			JButton jb = new JButton(Util.getIcon(perspective.getIconPath())); //$NON-NLS-1$ //$NON-NLS-2$
 			try{
 				jb.setToolTipText(Messages.getString("PerspectiveBarJPanel."+perspective.getID())); //$NON-NLS-1$
-				jb.setBorder(BorderFactory.createEmptyBorder(4,4,4,4));  //no border
 			}
 			catch(Exception e){  //ignore tooltip missing
 			}
@@ -110,8 +101,8 @@ public class PerspectiveBarJPanel extends JPanel implements ITechnicalStrings{
 				}
 			});
 			jtbPerspective.add(jb);
-			jtbPerspective.addSeparator();
-			alButtons.add(jb);
+    		alButtons.add(jb);
+            index ++;
 		}
 		add(jtbPerspective);
 	}
@@ -127,11 +118,12 @@ public class PerspectiveBarJPanel extends JPanel implements ITechnicalStrings{
 		while ( it.hasNext()){
 			final JButton jb = (JButton)it.next();
 			IPerspective perspective2 = (IPerspective)it2.next();
+            
 			if ( perspective2.equals(perspective)){  //this perspective is selected 
-			    jb.setBorder(BorderFactory.createLineBorder(Color.BLACK,4)); //this one is selected, black border, make it in the awt dispatcher thread!
+	            jb.setSelected(true);
 			}
 			else{
-			    jb.setBorder(BorderFactory.createEmptyBorder(4,4,4,4));  //no border
+	            jb.setSelected(false);
 			}
 		}
 	}
