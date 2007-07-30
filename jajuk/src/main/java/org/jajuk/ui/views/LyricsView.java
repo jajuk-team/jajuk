b7ef752a9c446cdce6c9267e569ec98658ca2be4
Bertrand Florat <bertrand@florat.net>
2007-07-30 21:53:22 +0000

    Various fixes anf UI enhancements

--- jajuk/src/main/java/org/jajuk/ui/views/LyricsView.java
+++ jajuk/src/main/java/org/jajuk/ui/views/LyricsView.java
@@ -45,13 +45,19 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+
+import com.jgoodies.forms.builder.PanelBuilder;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
 
 import ext.services.lyrics.LyricsService;
-
 
 /**
  * Lyrics view
@@ -64,6 +70,10 @@ public class LyricsView extends ViewAdapter implements Observer {
 	private static final long serialVersionUID = 2229941034734574056L;
 
 	private JTextArea textarea;
+
+	private JLabel jlTitle;
+
+	private JLabel jlAuthor;
 
 	/*
 	 * (non-Javadoc)
@@ -89,21 +99,43 @@ public class LyricsView extends ViewAdapter implements Observer {
 		textarea
 				.setFont(new Font("Dialog", Font.BOLD, ConfigurationManager.getInt(CONF_FONTS_SIZE)));
 		textarea.addMouseListener(new MouseAdapter() {
-		
+
 			@Override
 			public void mousePressed(MouseEvent e) {
-				if (e.getButton() == MouseEvent.BUTTON3){
+				if (e.isPopupTrigger()) {
 					JPopupMenu menu = new JPopupMenu();
 					menu.add(new JMenuItem(ActionManager.getAction(JajukAction.COPY_TO_CLIPBOARD)));
 					menu.add(new JMenuItem(ActionManager.getAction(JajukAction.LAUNCH_IN_BROWSER)));
 					menu.show(textarea, e.getX(), e.getY());
 				}
-					
+
 			}
-		
+
 		});
+		jlAuthor = new JLabel();
+		jlAuthor.setFont(new Font(Font.DIALOG, Font.PLAIN, ConfigurationManager
+				.getInt(CONF_FONTS_SIZE) + 2));
+		jlTitle = new JLabel();
+		jlTitle.setFont(new Font(Font.DIALOG, Font.PLAIN, ConfigurationManager
+				.getInt(CONF_FONTS_SIZE) + 4));
 		JScrollPane jsp = new JScrollPane(textarea);
-		add(jsp);
+		textarea.setFont(new Font(Font.DIALOG, Font.PLAIN, ConfigurationManager
+				.getInt(CONF_FONTS_SIZE)));
+		int height = getHeight() - 200;
+		FormLayout layout = new FormLayout(
+		// --columns
+				"3dlu,p:grow, 3dlu",
+				// --rows
+				"5dlu, p, 3dlu, p, 3dlu,"+height+":grow ,3dlu"); // rows
+		PanelBuilder builder = new PanelBuilder(layout);
+		CellConstraints cc = new CellConstraints();
+		// Add items
+		builder.add(jlTitle, cc.xy(2, 2));
+		builder.add(jlAuthor, cc.xy(2, 4));
+		builder.add(jsp, cc.xy(2, 6));
+		JPanel p = builder.getPanel();
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		add(p);
 
 		ObservationManager.register(this);
 		// check if a track has already been launched
@@ -130,32 +162,41 @@ public class LyricsView extends ViewAdapter implements Observer {
 	 * 
 	 * @see org.jajuk.base.Observer#update(org.jajuk.base.Event)
 	 */
-	public void update(Event event) {
-		EventSubject subject = event.getSubject();
-		if (subject.equals(EventSubject.EVENT_FILE_LAUNCHED)) {
-			File file = FIFO.getInstance().getCurrentFile();
-			if (file != null) {
-				Track track = FIFO.getInstance().getCurrentFile().getTrack();
-				String sURL = "http://www.lyrc.com.ar/en/tema1en.php?artist="
-						+ track.getAuthor().getName2() + "&songname=" + track.getName();
-				textarea.setToolTipText(sURL);
-				setText(LyricsService.getLyrics(track.getAuthor().getName2(), track.getName()));
-				Util.copyData = sURL;
-				try {
-					Util.url = new URL(sURL);
-				} catch (MalformedURLException e) {
-					Log.error(e);
+	public void update(final Event event) {
+		SwingUtilities.invokeLater(new Runnable() {
+
+			public void run() {
+				EventSubject subject = event.getSubject();
+				if (subject.equals(EventSubject.EVENT_FILE_LAUNCHED)) {
+					File file = FIFO.getInstance().getCurrentFile();
+					if (file != null) {
+						Track track = FIFO.getInstance().getCurrentFile().getTrack();
+						String sURL = "http://www.lyrc.com.ar/en/tema1en.php?artist="
+								+ track.getAuthor().getName2() + "&songname=" + track.getName();
+						textarea.setToolTipText(sURL);
+						setText(LyricsService.getLyrics(track.getAuthor().getName2(), track
+								.getName()));
+						jlAuthor.setText(track.getAuthor().getName2());
+						jlTitle.setText(track.getName());
+						Util.copyData = sURL;
+						try {
+							Util.url = new URL(sURL);
+						} catch (MalformedURLException e) {
+							Log.error(e);
+						}
+					}
+				} else if (subject.equals(EventSubject.EVENT_ZERO)) {
+					setText(Messages.getString("JajukWindow.18"));
+				} else if (subject.equals(EventSubject.EVENT_WEBRADIO_LAUNCHED)) {
+					WebRadio radio = (WebRadio) event.getDetails().get(DETAIL_CONTENT);
+					if (radio != null) {
+						setText(radio.getName());
+					}
 				}
 			}
-		} else if (subject.equals(EventSubject.EVENT_ZERO)) {
-			setText(Messages.getString("JajukWindow.18"));
-		}
-		else if (subject.equals(EventSubject.EVENT_WEBRADIO_LAUNCHED)) {
-			WebRadio radio = (WebRadio)event.getDetails().get(DETAIL_CONTENT);
-			if (radio != null) {
-				setText(radio.getName());
-			}
-		}
+
+		});
+
 	}
 
 	// update text area text
