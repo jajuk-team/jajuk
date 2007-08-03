04555b5b3619ee12180731aa09164cdf6d554f6b
Fsck222 <jajuk-developers@lists.sourceforge.net>
2007-08-03 19:31:29 +0000

    work on javahelp

--- jajuk/src/doc/jajuk-hs/default/jajuk.hs
+++ jajuk/src/doc/jajuk-hs/default/jajuk.hs
@@ -1,9 +1,9 @@
 <?xml version='1.0' encoding='ISO-8859-1' ?>
 <!DOCTYPE helpset
   PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
-         "./helpset_2_0.dtd">
+         "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
 
-<helpset version="1.0">
+<helpset version="2.0">
 
   <!-- title -->
   <title>Jajuk Help</title>
