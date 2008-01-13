ac07f9f187ba3c58d327cb196cb3fb86f0ba065e
Bertrand Florat <bertrand@florat.net>
2008-01-13 17:47:10 +0000

    Fix for lyrics busy label

--- jajuk/src/main/java/org/jajuk/util/Util.java
+++ jajuk/src/main/java/org/jajuk/util/Util.java
@@ -23,7 +23,6 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Cursor;
-import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Graphics2D;
 import java.awt.Image;
@@ -1128,8 +1127,6 @@ public class Util implements ITechnicalStrings {
       jpOut.add(jc);
       jpOut.add(Box.createVerticalGlue());
     }
-    jpOut.setMinimumSize(new Dimension(0, 0));
-    // allow resing with info node
     return jpOut;
   }
 
