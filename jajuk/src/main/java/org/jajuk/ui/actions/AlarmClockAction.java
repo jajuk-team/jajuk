e04e500308c01bdab85454dc14354e458c64d901
Bertrand Florat <bertrand@florat.net>
2008-12-23 21:32:41 +0000

    Finished alarm refactoring. Tested.

---
+++ jajuk/src/main/java/org/jajuk/ui/actions/AlarmClockAction.java
@@ -0,0 +1,46 @@
+/*
+ *  Jajuk
+ *  Copyright (C) 2005 The Jajuk Team
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *  $$Revision: 3156 $$
+ */
+package org.jajuk.ui.actions;
+
+import java.awt.event.ActionEvent;
+
+import org.jajuk.ui.widgets.AlarmClockDialog;
+import org.jajuk.util.IconLoader;
+import org.jajuk.util.JajukIcons;
+import org.jajuk.util.Messages;
+import org.jajuk.util.error.JajukException;
+
+/**
+ * Alarm wizard action
+ */
+public class AlarmClockAction extends JajukAction {
+
+  private static final long serialVersionUID = 1L;
+
+  AlarmClockAction() {
+    super(Messages.getString("AlarmClock.0"), IconLoader.getIcon(JajukIcons.ALARM), true);
+    setShortDescription(Messages.getString("AlarmClock.0"));
+  }
+
+  @Override
+  public void perform(ActionEvent evt) throws JajukException {
+    new AlarmClockDialog();
+  }
+}
