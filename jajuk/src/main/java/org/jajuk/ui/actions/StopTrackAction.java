cb079a365fcecc5677e747ea542df371bd10a5d3
Bertrand Florat <bertrand@florat.net>
2009-01-07 21:42:05 +0000

    Player labels cleanup

--- jajuk/src/main/java/org/jajuk/ui/actions/StopTrackAction.java
+++ jajuk/src/main/java/org/jajuk/ui/actions/StopTrackAction.java
@@ -30,7 +30,7 @@ public class StopTrackAction extends JajukAction {
   private static final long serialVersionUID = 1L;
 
   StopTrackAction() {
-    super(Messages.getString("JajukWindow.11"), IconLoader.getIcon(JajukIcons.STOP), "ctrl S",
+    super(Messages.getString("JajukWindow.27"), IconLoader.getIcon(JajukIcons.STOP), "ctrl S",
         false, false);
     setShortDescription(Messages.getString("JajukWindow.27"));
   }
