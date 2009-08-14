9ad699beb90b83d7f1ab15721071b91897000f85
Dominik Stadler <jajuk-developers@lists.sourceforge.net>
2009-08-14 10:13:51 +0000

    new small test for this DebugLogAction

---
+++ jajuk/src/test/java/org/jajuk/ui/actions/TestDebugLogAction.java
@@ -0,0 +1,56 @@
+/*
+ *  Jajuk
+ *  Copyright (C) 2003-2009 The Jajuk Team
+ *  http://jajuk.info
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *  $Revision: 3132 $
+ */
+package org.jajuk.ui.actions;
+
+import java.awt.HeadlessException;
+
+import org.jajuk.util.log.Log;
+
+import junit.framework.TestCase;
+
+/**
+ * 
+ */
+public class TestDebugLogAction extends TestCase {
+
+  /**
+   * Test method for {@link org.jajuk.ui.actions.DebugLogAction#perform(java.awt.event.ActionEvent)}.
+   */
+  public void testPerform() {
+    // initialize Log
+    assertNotNull(Log.getInstance());
+    
+    DebugLogAction action = new DebugLogAction();
+    try {
+    action.perform(null); // it doesn't look at the actionevent at all right now...
+    } catch (HeadlessException e) {
+      // thrown on Hudson/Sonar as they do not support tests that require UI
+    }
+  }
+
+  /**
+   * Test method for {@link org.jajuk.ui.actions.DebugLogAction#DebugLogAction()}.
+   */
+  public void testDebugLogAction() {
+    new DebugLogAction();
+  }
+
+}
