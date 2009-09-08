f101c19c778c0d657595c6f8b87cca1a8c86bb9b
Dominik Stadler <jajuk-developers@lists.sourceforge.net>
2009-09-08 20:27:35 +0000

    new test for ExitService

---
+++ jajuk/src/test/java/org/jajuk/services/core/TestExitService.java
@@ -0,0 +1,72 @@
+/*
+ *  Jajuk
+ *  Copyright (C) 2003-2009 The Jajuk Team
+ *  http://jajuk.info
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *  $Revision: 3132 $
+ */
+package org.jajuk.services.core;
+
+import junit.framework.TestCase;
+
+import org.jajuk.services.startup.StartupCollectionService;
+
+/**
+ * 
+ */
+public class TestExitService extends TestCase {
+
+  /**
+   * Test method for {@link org.jajuk.services.core.ExitService#run()}.
+   * @throws Exception 
+   */
+  public void testRun() throws Exception {
+    StartupCollectionService.registerItemManagers();
+    
+    ExitService service = new ExitService();
+    service.run();
+    
+    // once again with sessionidfile
+    SessionService.getSessionIdFile().createNewFile();
+    service.run();
+
+  }
+
+  /**
+   * Test method for {@link org.jajuk.services.core.ExitService#ExitService()}.
+   */
+  public void testExitService() {
+    new ExitService();
+  }
+
+  /**
+   * Test method for {@link org.jajuk.services.core.ExitService#exit(int)}.
+   */
+  public void testExit() {
+    ExitService service = new ExitService();
+    assertNotNull(service);
+    
+    // don't run this as it stops the JVM! service.exit(1);
+  }
+
+  /**
+   * Test method for {@link org.jajuk.services.core.ExitService#isExiting()}.
+   */
+  public void testIsExiting() {
+    assertFalse(ExitService.isExiting());
+  }
+
+}
