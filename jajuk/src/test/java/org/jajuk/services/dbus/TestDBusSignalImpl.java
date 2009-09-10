57caca556f03ad1aa94375ae2dbbc69fb0ac92a2
Dominik Stadler <jajuk-developers@lists.sourceforge.net>
2009-09-10 06:41:02 +0000

    test case for DBusSignalImpl

---
+++ jajuk/src/test/java/org/jajuk/services/dbus/TestDBusSignalImpl.java
@@ -0,0 +1,44 @@
+/*
+ *  Jajuk
+ *  Copyright (C) 2003-2009 The Jajuk Team
+ *  http://jajuk.info
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *  $Revision: 3132 $
+ */
+package org.jajuk.services.dbus;
+
+import junit.framework.TestCase;
+
+/**
+ * 
+ */
+public class TestDBusSignalImpl extends TestCase {
+
+  /**
+   * Test method for {@link org.jajuk.services.dbus.DBusSignalImpl#isRemote()}.
+   */
+  public final void testIsRemote() {
+    // currently false
+    assertFalse(new DBusSignalImpl().isRemote());
+  }
+
+  public final void testFileChangedSignal() throws Exception {
+    DBusSignalImpl.FileChangedSignal signal = 
+      new DBusSignalImpl.FileChangedSignal("testfile", "/path/test");
+    
+    assertEquals("testfile", signal.getFilename());
+  }
+}
