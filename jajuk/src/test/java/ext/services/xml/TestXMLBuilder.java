f3663a9779ecd71bca30ae9a171aede6b0fe7578
Dominik Stadler <jajuk-developers@lists.sourceforge.net>
2009-10-09 12:42:55 +0000

    new testcase

---
+++ jajuk/src/test/java/ext/services/xml/TestXMLBuilder.java
@@ -0,0 +1,51 @@
+/*
+ *  Jajuk
+ *  Copyright (C) 2003-2009 The Jajuk Team
+ *  http://jajuk.info
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *  $Revision: 3132 $
+ */
+package ext.services.xml;
+
+import org.jajuk.JUnitHelpers;
+import org.jajuk.JajukTestCase;
+
+/**
+ * 
+ */
+public class TestXMLBuilder extends JajukTestCase {
+
+  /**
+   * Test method for {@link ext.services.xml.XMLBuilder#getXMLDocument(java.lang.String)}.
+   */
+  public void testGetXMLDocument() {
+    // simple xml...
+    assertNotNull(XMLBuilder.getXMLDocument("<xml></xml>"));
+    
+    // empty values should be silently ignored
+    assertNull(XMLBuilder.getXMLDocument(null));
+    assertNull(XMLBuilder.getXMLDocument(""));
+    
+    // some invalid XML should cause an exception internally but report null
+    assertNull(XMLBuilder.getXMLDocument("<xml>invliad document without end tag..."));
+  }
+
+  // helper method to emma-coverage of the unused constructor
+  public void testPrivateConstructor() throws Exception {
+    JUnitHelpers.executePrivateConstructor(XMLBuilder.class); 
+  }
+  
+}
