d661a0be53c40cf16443585dc99a17980936996a
Bertrand Florat <bertrand@florat.net>
2009-11-02 22:26:05 +0000

    * code formating * Removed system.err.println

--- jajuk/src/main/java/org/jajuk/services/notification/JajukBalloonNotificator.java
+++ jajuk/src/main/java/org/jajuk/services/notification/JajukBalloonNotificator.java
@@ -1,19 +1,15 @@
-
 package org.jajuk.services.notification;
 
 import org.jajuk.ui.widgets.JajukTrackChangeBalloon;
 
-public class JajukBalloonNotificator implements ISystemNotificator
-{
-	@Override
-	public boolean isAvailable()
-	{
-		return true;
-	}
+public class JajukBalloonNotificator implements ISystemNotificator {
+  @Override
+  public boolean isAvailable() {
+    return true;
+  }
 
-	@Override
-	public void notify(String title, String text)
-	{System.err.println("NOTIFY");
-		new JajukTrackChangeBalloon(text, 3000).display();
-	}
+  @Override
+  public void notify(String title, String text) {
+    new JajukTrackChangeBalloon(text, 3000).display();
+  }
 }
