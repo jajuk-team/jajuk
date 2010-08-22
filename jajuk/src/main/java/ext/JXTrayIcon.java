2810f0f5fe03282ae8bbec7ce1750d6fed5531af
Bertrand Florat <bertrand@florat.net>
2010-08-22 20:55:48 +0000

    Code cleanup, remove demo code from this class

--- jajuk/src/main/java/ext/JXTrayIcon.java
+++ jajuk/src/main/java/ext/JXTrayIcon.java
@@ -6,7 +6,7 @@
  *  Code modified from Alexander Potochkin's JXTray class at 
  *  https://swinghelper.dev.java.net/source/browse/swinghelper/src/java/org/jdesktop/swinghelper/tray/JXTrayIcon.java?view=markup
  *  Copyright 2008 Sun Microsystems, Inc., 4150 Network Circle,
- * Santa Clara, California 95054, U.S.A. All rights reserved.
+ *  Santa Clara, California 95054, U.S.A. All rights reserved.
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
@@ -26,29 +26,15 @@
 
 package ext;
 
-import java.awt.AWTException;
-import java.awt.Color;
 import java.awt.Frame;
-import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.SystemTray;
 import java.awt.TrayIcon;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
-import java.awt.geom.Ellipse2D;
-import java.awt.image.BufferedImage;
 
 import javax.swing.JDialog;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
 import javax.swing.event.PopupMenuEvent;
 import javax.swing.event.PopupMenuListener;
-
-import org.jajuk.util.log.Log;
 
 /**
  * This class allow to add a Swing JDialog into an AWT Systray
@@ -130,75 +116,5 @@ public class JXTrayIcon extends TrayIcon {
     menu.addPopupMenuListener(popupListener);
   }
 
-  /**
-   * Creates the gui.
-   * DOCUMENT_ME
-   */
-  private static void createGui() {
-    JXTrayIcon tray = new JXTrayIcon(createImage());
-    tray.setJPopuMenu(createJPopupMenu());
-    try {
-      SystemTray.getSystemTray().add(tray);
-    } catch (AWTException e) {
-      Log.error(e);
-    }
-  }
-
-  /**
-   * The main method.
-   * 
-   * @param args the arguments
-   * 
-   * @throws Exception the exception
-   */
-  public static void main(String[] args) throws Exception {
-
-    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-
-    SwingUtilities.invokeLater(new Runnable() {
-      public void run() {
-        createGui();
-      }
-    });
-  }
-
-  /**
-   * Creates the image.
-   * DOCUMENT_ME
-   * 
-   * @return the image
-   */
-  static Image createImage() {
-    BufferedImage i = new BufferedImage(32, 32, BufferedImage.TYPE_INT_ARGB);
-    Graphics2D g2 = (Graphics2D) i.getGraphics();
-    g2.setColor(Color.RED);
-    g2.fill(new Ellipse2D.Float(0, 0, i.getWidth(), i.getHeight()));
-    g2.dispose();
-    return i;
-  }
-
-  /**
-   * Creates the j popup menu.
-   * DOCUMENT_ME
-   * 
-   * @return the j popup menu
-   */
-  static JPopupMenu createJPopupMenu() {
-    final JPopupMenu m = new JPopupMenu();
-    m.add(new JMenuItem("Item 1"));
-    m.add(new JMenuItem("Item 2"));
-    JMenu submenu = new JMenu("Submenu");
-    submenu.add(new JMenuItem("item 1"));
-    submenu.add(new JMenuItem("item 2"));
-    submenu.add(new JMenuItem("item 3"));
-    m.add(submenu);
-    JMenuItem exitItem = new JMenuItem("Exit");
-    exitItem.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        System.exit(0);
-      }
-    });
-    m.add(exitItem);
-    return m;
-  }
+    
 }
