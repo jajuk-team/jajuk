ad7852831b76a46439506fd3aba4887c49d6e470
Bertrand Florat <bertrand@florat.net>
2011-01-02 16:57:50 +0100

    Fix for #1743 (Few legals notices issues)

--- jajuk/src/main/java/ext/scrollablepopupmenu/XCheckedButton.java
+++ jajuk/src/main/java/ext/scrollablepopupmenu/XCheckedButton.java
@@ -1,22 +1,7 @@
-/*
- *  Jajuk
- *  Copyright (C) 2003-2010 The Jajuk Team
- *  http://jajuk.info
- *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *  $Revision$
+/**
+ * @author balajihe from
+ *         http://www.beginner-java-tutorial.com/scrollable-jpopupmenu.html
+ * 
  */
 package ext.scrollablepopupmenu;
 
