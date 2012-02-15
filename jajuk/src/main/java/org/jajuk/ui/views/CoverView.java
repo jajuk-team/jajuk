c6556584e663e088dad2f2325bb6723d25700889
Bertrand Florat <bertrand@florat.net>
2012-02-15 21:59:12 +0100

    Fix a regression due to #1821 (Cover resize force a new cover in shuffle mode) fix, race condition.

--- jajuk/src/main/java/org/jajuk/ui/views/CoverView.java
+++ jajuk/src/main/java/org/jajuk/ui/views/CoverView.java
@@ -763,7 +763,13 @@ public class CoverView extends ViewAdapter implements ComponentListener, ActionL
    */
   @Override
   public void componentResized(final ComponentEvent e) {
-    // manualResize = true;
+    final long lCurrentDate = System.currentTimeMillis(); // adjusting code
+    if (lCurrentDate - lDateLastResize < 500) { // Do consider only one event every 
+      // 500 ms to avoid race conditions and lead to unexpected states (verified)
+      lDateLastResize = lCurrentDate;
+      return;
+    }
+    lDateLastResize = lCurrentDate;
     Log.debug("Cover resized");
     // Force initial cover refresh. We do this inside this method and not initUI() 
     // because we make sure that the window is fully displayed then (otherwise, we get 
@@ -844,7 +850,6 @@ public class CoverView extends ViewAdapter implements ComponentListener, ActionL
    * @param index index of the cover to display
    */
   private synchronized void displayCover(final int index) {
-    // manualResize = false;
     if ((alCovers.size() == 0) || (index >= alCovers.size()) || (index < 0)) {
       // just a check
       alCovers.add(CoverView.nocover); // display nocover by default
