54c0fd8db8487b554e6397f71c527f8ba7b489c9
Bertrand Florat <bertrand@florat.net>
2012-08-07 22:01:39 +0200

    Attempt to fix a OSX issue : title view truncated in full screen view

--- jajuk/src/main/java/org/jajuk/ui/windows/JajukFullScreenWindow.java
+++ jajuk/src/main/java/org/jajuk/ui/windows/JajukFullScreenWindow.java
@@ -272,7 +272,7 @@ public class JajukFullScreenWindow extends JWindow implements IJajukWindow {
     setLayout(new MigLayout("ins 0", "[grow]", "[][grow][70%!][][]"));
     add(jbFull, "right,split 2,gapright 5");
     add(jbExit, "right,wrap");
-    add(animationView, "alignx center,aligny top,grow,gap bottom 20,wrap");
+    add(animationView, "alignx center,aligny top,grow,wrap");
     add(coverView, "alignx center, grow,gap bottom 20,wrap");
     add(jtbPlay, "alignx center,gap bottom 20,wrap");
     add(tpst, "alignx center,width 50%!,aligny bottom,gap bottom 10");
