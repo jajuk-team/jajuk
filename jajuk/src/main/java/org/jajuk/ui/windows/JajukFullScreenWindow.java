01b287ab771259d8ad1530474d902b1e9d15fda7
Bertrand Florat <bertrand@florat.net>
2012-08-20 22:17:50 +0200

    Attempt to fix bug Ticket #1851 (Taskbar visible in full screen mode), fixed under Linux

--- jajuk/src/main/java/org/jajuk/ui/windows/JajukFullScreenWindow.java
+++ jajuk/src/main/java/org/jajuk/ui/windows/JajukFullScreenWindow.java
@@ -228,7 +228,7 @@ public class JajukFullScreenWindow extends JWindow implements IJajukWindow {
    * Instantiates a new jajuk full screen window.
    */
   public JajukFullScreenWindow() {
-    super(owner);
+    super();
     setAlwaysOnTop(true);
     // Add Mouse Listener to disable mouse cursor
     addMouseListener(new MouseAdapter() {
